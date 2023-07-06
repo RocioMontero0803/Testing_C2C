c470ff77484707155f7620c2a03e0edc5653fade
RocioMontero0803 <Rocio.montero0803@gmail.com>
2023-07-06 10:03:27 -0500

    Profile data saved on firebase

--- app/src/main/java/com/example/testing_/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/testing_/ui/slideshow/SlideshowFragment.kt
@@ -1,17 +1,32 @@
 package com.example.testing_.ui.slideshow
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsClient.getPackageName
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.testing_.R
+import com.example.testing_.User
 import com.example.testing_.databinding.FragmentSlideshowBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class SlideshowFragment : Fragment() {
 
     private var _binding: FragmentSlideshowBinding? = null
+    private lateinit var auth : FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var storageReference: StorageReference
+    private lateinit var imageUri: Uri
+    //private val packageName = context?.packageName
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,12 +42,41 @@ class SlideshowFragment : Fragment() {
 
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        auth = FirebaseAuth.getInstance()
+        val uid = auth.currentUser?.uid
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+        binding.sBtn.setOnClickListener{
+            val name = binding.etName.text.toString()
+            val status = binding.etStatus.text.toString()
 
+            val user = User(name,status)
+
+            if(uid != null){
+                databaseReference.child(uid).setValue(user).addOnCompleteListener{
+
+                    if (it.isSuccessful){
+                        uploadProfilePic()
+                    }else{
+                        Toast.makeText(context, "Failed to update profile", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 //        val textView: TextView = binding.textSlideshow
 //        slideshowViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
         return root
+    }
+
+    private fun uploadProfilePic() {
+       imageUri = Uri.parse("android.resource://" + requireContext().packageName +"/${R.drawable.profile_icon}")
+        storageReference = FirebaseStorage.getInstance().getReference("Users/"+auth.currentUser?.uid)
+        storageReference.putFile(imageUri).addOnSuccessListener {
+            Toast.makeText(context, "Profile success", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener{
+            Toast.makeText(context, "Failed to upload image", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
